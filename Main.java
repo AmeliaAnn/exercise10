5702f6986fcd8061742829933a00035d336526e2
AmeliaAnn <53219102+AmeliaAnn@users.noreply.github.com>
2019-10-26 15:23:28 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,32 +13,24 @@ public class Main {
                 
         System.out.print("Введите количество столбцов матрицы: ");
       int  cols = in.nextInt();
+      
+  int[][] arr = new int[rows][cols];
+
+   for (int i = 0; i < arr.length; i++) {
+   for (int j = 0; j < arr[i].length; j++) {
+System.out.print("Введите элемент arr[" + i + "][" + j + "]:");
+arr[i][j] = in.nextInt();
        
- 
-   int[][] arr = new int[rows][cols];
-        
-        for (int i = 0; i < arr.length; i++) 
-        {
-            for (int j = 0; j < arr[i].length; j++)          {
-           
-                System.out.print("Введите элемент arr[" + i + "][" + j + "]:");
-               
-                arr[i][j] = in.nextInt();
-                
-                in.close();
-              }
-           }
-                
-               while (i >=arr.length)
-               
-              {
-                
-                 [i][j] *= 3; 
-                
-               
-              System.out.print("1 строка умножена на 3: arr[" + i + "][" + j + "]:");
-        
-       }
-     }  
-  }
-     
+    
+in.close();
+  
+  
+  do (i * 3) {
+  System.out.println("1 строка умноженная на 3: arr[" + i + "][" + j + "]:");
+  i++;
+       } while (arr[0][j]);
+      }
+     }
+    }
+    
+}
